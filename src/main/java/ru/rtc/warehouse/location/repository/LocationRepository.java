b8e84d235488d1db1a67085d1c000b9e7f471f97
Bo1dos <sasaasihmin06@gmail.com>
2025-11-02 01:49:49 +0300

    feat(location): add Location and LocationStatus domain, repos, services and DB changelogs

--- src/main/java/ru/rtc/warehouse/location/repository/LocationRepository.java
+++ src/main/java/ru/rtc/warehouse/location/repository/LocationRepository.java
@@ -6,8 +6,12 @@ import ru.rtc.warehouse.location.model.Location;
 import ru.rtc.warehouse.warehouse.model.Warehouse;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface LocationRepository extends JpaRepository<Location, Long> {
 	List<Location> findByWarehouse(Warehouse warehouse);
+	Optional<Location> findByWarehouseAndZoneAndRowAndShelf(Warehouse warehouse, Integer zone, Integer row,
+			Integer shelf);
+	
 }
