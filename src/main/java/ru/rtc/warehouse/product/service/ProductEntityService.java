488557ad4e95cf364a2b97dd19b0de6aaa557378
Семенюта Никита Владимирович <n.semenyuta@bft.ru>
2026-06-03 20:45:12 +0300

    feature/product Работа с отдельными подуктами

--- src/main/java/ru/rtc/warehouse/product/service/ProductEntityService.java
+++ src/main/java/ru/rtc/warehouse/product/service/ProductEntityService.java
@@ -1,15 +1,16 @@
 package ru.rtc.warehouse.product.service;
 
+import java.util.List;
 import ru.rtc.warehouse.common.CrudEntityService;
 import ru.rtc.warehouse.product.model.Product;
 
-import java.util.List;
-
 public interface ProductEntityService extends CrudEntityService<Product, Long> {
-	Product findBySkuCode(String skuCode);
-	List<Product> findAllActiveProducts();
-	Long count();
-	Product findByNameAndCategory(String name, String category);
+    Product findBySkuCode(String skuCode);
+    List<Product> findAllActiveProducts();
+    Long count();
+    Product findByNameAndCategory(String name, String category);
 
-	List<Product> findAllByWarehouseCode(String warehouseCode);
+    List<Product> findAllByWarehouseCode(String warehouseCode);
+
+    List<String> findDistinctCategories();
 }
